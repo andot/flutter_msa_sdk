e308e93aab1d63dce30b2e5f011acbe5736857cc
andot <mabingyao@gmail.com>
2020-04-15 15:02:05 +0800

    Upgrade Android X #3

--- android/src/main/java/com/adtalos/flutter/msa/sdk/FlutterMsaSdkPlugin.java
+++ android/src/main/java/com/adtalos/flutter/msa/sdk/FlutterMsaSdkPlugin.java
@@ -1,7 +1,7 @@
 package com.adtalos.flutter.msa.sdk;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 
 import com.bun.miitmdid.core.JLibrary;
 
